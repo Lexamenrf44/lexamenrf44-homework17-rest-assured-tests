16c13d04acf1b1920781f4836bf40e7955f77bbf
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-09 23:02:23 +0300

    created new repository, created new branch, created new files, configured build.gradle and .gitignore

---
+++ src/test/java/in/regres/RegresTests.java
@@ -0,0 +1,107 @@
+package in.regres;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class RegresTests {
+
+    @Test
+    void listUsers() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("total_pages", is(2))
+                .body("data.email[1]", is("lindsay.ferguson@reqres.in"));
+    }
+
+    @Test
+    void createUser() {
+        String req_data = "{ \"name\": \"Alexander\", \"job\": \"Tester\" }";
+        given()
+                .contentType(JSON)
+                .body(req_data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("Alexander"))
+                .body("job", is("Tester"));
+    }
+
+    @Test
+    void deleteUser() {
+        given()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void successfulEmailRegister() {
+        String req_data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+
+        given()
+                .contentType(JSON)
+                .body(req_data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessfulRegister() {
+        String data = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void successfulLogin() {
+
+        String req_data = "{ \"email\": \"eve.holt@reqres.in\", " +
+                        "\"password\": \"cityslicka\" }";
+
+        given()
+                .contentType(JSON)
+                .body(req_data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void negativeLogin() {
+
+        String req_data = "{ \"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .contentType(JSON)
+                .body(req_data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+
+
+}
--- src/test/java/in/regres/regresTests.java
+++
@@ -1,6 +0,0 @@
-package in.regres;
-
-public class regresTests {
-
-
-}
