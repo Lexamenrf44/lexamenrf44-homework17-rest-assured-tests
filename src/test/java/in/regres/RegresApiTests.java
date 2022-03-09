d039e020cd9c384e2448cb2b81a7dfe7bf68df55
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-09 23:03:22 +0300

    renamed RegresTests.java into RegresApiTests.java, edited RegresApiTests.java

--- src/test/java/in/regres/RegresApiTests.java
+++ src/test/java/in/regres/RegresApiTests.java
@@ -6,7 +6,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
-public class RegresTests {
+public class RegresApiTests {
 
     @Test
     void listUsers() {
