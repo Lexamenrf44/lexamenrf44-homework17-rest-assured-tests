a6d7605f2efcfcd45fc7094a6823d7caafec2b6f
Lexamenrf44 <alexander.stcstudent@gmail.com>
2022-03-10 08:25:04 +0300

    edited RegresApiTests.java, refactored the check from exact value to notNullValue

--- src/test/java/in/regres/RegresApiTests.java
+++ src/test/java/in/regres/RegresApiTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class RegresApiTests {
 
@@ -54,7 +55,7 @@ public class RegresApiTests {
                 .then()
                 .statusCode(200)
                 .body("id", is(4))
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .body("token", notNullValue());
     }
 
     @Test
@@ -84,7 +85,7 @@ public class RegresApiTests {
                 .post("https://reqres.in/api/login")
                 .then()
                 .statusCode(200)
-                .body("token", is("QpwL5tke4Pnpja7X4"));
+                .body("token", notNullValue());
     }
 
     @Test
